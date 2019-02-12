96b2bf825e468478ec88dac6b9f0176dccc47d1b
piyush <piyushratan9451@gmail.com>
2019-02-12 20:37:54 +0530

    Changed database.php

--- config/database.php
+++ config/database.php
@@ -41,12 +41,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '35.200.255.234'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => env('DB_DATABASE', 'test'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'examdate90'),
+            'unix_socket' => env('DB_SOCKET', '/cloudsql/project-examdate:asia-south1:mysql-for-examdate'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
